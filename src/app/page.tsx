c16522c6e73c394b2e6731b6c155a5864a1214ac
Arnav Saxena <arnavsaxena221@gmail.com>
2025-03-26 18:52:39 +0530

    More UI updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Hero from "@/components/hero";
+import Grid from "@/components/grid";
 import {FloatingNav} from "@/components/ui/floating-navbar";
 import {FaHouse} from "react-icons/fa6";
 import {FaContao, FaInfo} from "react-icons/fa";
-
 
 
 export default function Home() {
@@ -11,7 +11,8 @@ export default function Home() {
             className="bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
             <div className="max-w-7xl w-full">
                 <FloatingNav navItems={navItems}/>
-                <Hero />
+                <Hero/>
+                <Grid/>
             </div>
         </main>
     );
